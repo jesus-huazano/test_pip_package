847cc4fb1d8da48ca12de00f5ecb53c163c5991d
Txus <txus@valholl.local>
2021-10-12 12:53:21 -0500

    first commit

---
+++ build/lib/libname/hello.py
@@ -0,0 +1,5 @@
+def say_hello(): 
+    print("hello world")
+
+def say_hello2(text):
+    print("Hola {text}")
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.0'
+DESCRIPTION = 'Creacion y suscripcion a SNS'
+LONG_DESCRIPTION = ''
+
+# Setting up
+setup(
+    name="rnkmi-sns",
+    version=VERSION,
+    author="Jesus Huazano (jesus.huazano)",
+    author_email="<jesus.huazano@rankmi.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['SNS'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Renkmi Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
